10ac7b1c92fe7f0da3130fee8e3eec8578d4f6ab
Srinivas Joshi <69837749+SrinivasJoshi@users.noreply.github.com>
2021-03-27 12:52:59 +0530

    Added: 1- responsiveness 2-input and button styling

--- src/Login.js
+++ src/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import img1 from './images/student-login-img.svg';
 import img2 from './images/professional-login-img.svg';
 
@@ -23,9 +23,14 @@ const Login = (props) => {
     const handleSubmit=(e)=>{
         e.preventDefault();
     }
+    let [display1, setDisplay1] = useState(false);
+    const toggleDiv = (e)=>{
+        e.preventDefault();
+        setDisplay1(display1=!display1)
+    }
     return (
         <div className="Login">
-            <div className="two">
+            <div className={`two ${display1?'':'dispOff'}`}>
                 <h2>Login for College Students</h2>
                 <img src={img1} alt=""/>
                 <p>Find people and peer groups with same niche as yours </p>
@@ -42,8 +47,9 @@ const Login = (props) => {
                     <p className="errorMsg">{password2Error}</p>
                     <button type="button" onClick={handleLoginClg}>Login</button>
                 </form>
+                <p className="toggle-link" onClick={toggleDiv}><a href="">Login as a Professional?</a></p>
             </div>
-            <div className="one">
+            <div className={`one ${display1?'dispOff':''}`}>
                 <h2>Login for Professionals</h2>
                 <img src={img2} alt=""/>
                 <p>Meet the professionals all around the world!</p>
@@ -60,6 +66,7 @@ const Login = (props) => {
                     <p className="errorMsg">{password1Error}</p>
                     <button type="submit" onClick={handleLogin}>Login</button>
                 </form>
+                <p className="toggle-link" onClick={toggleDiv}><a href="">Login as a Student?</a></p>
             </div> 
         </div>
     );
